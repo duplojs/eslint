5fb8257109e4182c9cd873c441c4fecf5c0f15db
mathcovax <campani.mathieu@gmail.com>
2024-10-30 20:31:37 +0000

    fix: test tu

--- scripts/index.test.ts
+++ scripts/index.test.ts
@@ -1,15 +1,51 @@
 import { Linter, type FlatConfig } from "@typescript-eslint/utils/ts-eslint";
-import configEslint from ".";
+import { duplojsEslintBase, duplojsEslintOpen, duplojsEslintTest } from ".";
 import { resolve } from "path";
 
 describe("eslint rule", () => {
-	it("all rules exist ", () => {
+	it("all rules exist base", () => {
 		const linter = new Linter();
 
 		const tempConfigEslint: FlatConfig.Config = {
-			...configEslint,
+			...duplojsEslintBase,
 			languageOptions: {
-				...configEslint.languageOptions,
+				...duplojsEslintBase.languageOptions,
+				parserOptions: {
+					project: "tsconfig.json",
+					tsconfigRootDir: resolve(import.meta.dirname, ".."),
+				},
+			},
+			files: ["scripts/**/*.ts"],
+		};
+
+		expect(() => linter.verify("\n", tempConfigEslint)).not.toThrowError();
+	});
+
+	it("all rules exist open", () => {
+		const linter = new Linter();
+
+		const tempConfigEslint: FlatConfig.Config = {
+			...duplojsEslintOpen,
+			languageOptions: {
+				...duplojsEslintOpen.languageOptions,
+				parserOptions: {
+					project: "tsconfig.json",
+					tsconfigRootDir: resolve(import.meta.dirname, ".."),
+				},
+			},
+			files: ["scripts/**/*.ts"],
+		};
+
+		expect(() => linter.verify("\n", tempConfigEslint)).not.toThrowError();
+	});
+
+	it("all rules exist test", () => {
+		const linter = new Linter();
+
+		const tempConfigEslint: FlatConfig.Config = {
+			...duplojsEslintTest,
+			languageOptions: {
+				...duplojsEslintTest.languageOptions,
 				parserOptions: {
 					project: "tsconfig.json",
 					tsconfigRootDir: resolve(import.meta.dirname, ".."),
