bbcf4938e7c11b94832e00b29693e21f859c1fdb
mathcovax <campani.mathieu@gmail.com>
2024-10-30 20:07:59 +0000

    feat: multie rules

---
+++ scripts/open.ts
@@ -0,0 +1,20 @@
+import { base } from "./base";
+import type { FlatConfig } from "@typescript-eslint/utils/ts-eslint";
+
+export const open = {
+	...base,
+	rules: {
+		...base.rules,
+		"no-eval": "off",
+		"no-bitwise": "off",
+		"new-cap": "off",
+		"func-style": "off",
+		"max-classes-per-file": "off",
+		"@typescript-eslint/no-explicit-any": "off",
+		"@typescript-eslint/use-unknown-in-catch-callback-variable": "off",
+		"@typescript-eslint/await-thenable": "off",
+		"@typescript-eslint/no-empty-object-type": "off",
+		"@typescript-eslint/no-magic-numbers": "off",
+		"@typescript-eslint/no-use-before-define": "off",
+	},
+} as const satisfies FlatConfig.Config;
