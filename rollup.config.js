7d81c48a6b8f61225ba143c60ac8abd5ebe2082f
mathcovax <campani.mathieu@gmail.com>
2025-08-16 18:35:30 +0000

    fix: update ci

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,23 @@
+import typescript from '@rollup/plugin-typescript';
 import { defineConfig } from "rollup";
-import esbuild from "rollup-plugin-esbuild";
 
 export default defineConfig({
-		input: "scripts/index.ts",
-		output: [
-			{
-				file: "dist/index.cjs",
-				format: "cjs",
-			},
-			{
-				file: "dist/index.mjs",
-				format: "esm",
-			}
-		],
-		plugins: [
-			esbuild({
-				tsconfig: "tsconfig.json",
-				include: /\.[jt]sx?$/,
-				exclude: /node_modules/,
-			})
-		]
-})
+	input: "scripts/index.ts",
+	output: [
+		{
+			file: "dist/index.cjs",
+			format: "cjs",
+		},
+		{
+			file: "dist/index.mjs",
+			format: "esm",
+		},
+	],
+	plugins: [
+		typescript({
+			tsconfig: "tsconfig.build.json",
+			include: /\.[jt]sx?$/,
+			exclude: /node_modules/,
+		}),
+	],
+});
